d69cc8f81b47d3303b07781c011867c3e7289ebe
Cedric <cedricmarsberg@gmail.com>
2025-10-28 22:28:48 +0100

    Projekte A, B und h3 hinzugefügt

---
+++ h2/H2_main.java
@@ -0,0 +1,32 @@
+package h2;
+
+public class H2_main {
+public static void main(String[] args) {
+int i = 5, j = 20, k =8, min =10, max = 50;
+
+min = j; 
+        
+        if (i < min) {
+            min = i;
+        }
+        
+        if (k < min) {
+            min = k;
+        }
+
+        max = i;
+        
+        if (j > max) {
+            max = j;
+        }
+        
+        if (k > max) {
+            max = k;
+        }
+
+        
+    
+
+}
+}
+
---
+++ p2/P2_main.java
@@ -0,0 +1,17 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+
+        int i = 9;
+        boolean b = false;
+        System.out.println("Startwert i: " + i);
+        System.out.println("Startwert b: " + b);
+
+        if (i > 10) {
+            b = true;
+        }
+        System.out.println("Endwert b: " + b);
+
+    }
+}
