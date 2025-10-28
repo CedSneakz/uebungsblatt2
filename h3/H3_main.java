9ad04d7fb4db773fd762f3f3d796e5a9a3c490da
Cedric <cedricmarsberg@gmail.com>
2025-10-28 22:39:05 +0100

    Alle Projektordner hinzugefügt

---
+++ h3/H3_main.java
@@ -0,0 +1,22 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 2220, j = 9, k= 50;
+
+        boolean A1 = (i > j);
+        boolean A2 = (i > 200);
+        boolean A3 = (j > 100);
+
+        if (A1 && !A2 && !A3) {
+            k = 1;
+        } else if (A1 && A2 && !A3) {
+            k = 2;
+        } else if (A1 && A2 && A3) {
+            k = 3;
+        } else if (!A1 && !A2 && !A3) {
+            k = 4;
+        }
+       System.out.println(k);
+    }
+}
